e9fea72fd48c5bdd188714051fb66f0f9db9c893
muzafferse <muzaffer.sevili@vestel.com.tr>
2025-03-02 22:04:35 +0300

    delete unused code

--- Clinical Psychology/AppCordinator.swift
+++ Clinical Psychology/AppCordinator.swift
@@ -12,13 +12,6 @@ class AppCoordinator: NSObject, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // Handle app Did Finish Launching With Options
         FirebaseGateway.configure()
-#if CLINIC1A
-        print("CLINIC1A")
-#elseif CLINIC1B
-        print("CLINIC1B")
-#else
-        print("OTHERS")
-#endif
         return true
     }
     
